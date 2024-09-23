5c0481fa88b3fc2da6e293e0bf2b8a264310ffe0
etrnldspr <92818272+etrnldspr@users.noreply.github.com>
2024-09-23 22:54:56 +0300

    4

--- lab_4.jl
+++ lab_4.jl
@@ -1,18 +1,53 @@
-function lab_4(r::Robot)
-    path = go_to_west_south_corner_and_return_path!(r; go_around_barriers = false)
-
-    x = go_to_border_come_back_and_return_distance!(r, East; go_around_barriers = false)
-    marks_to_do = x
-    while !isborder(r,Nord) && marks_to_do > 0
-        go!(r, East; steps = marks_to_do, go_around_barriers = false, markers = true)
-        go!(r, West; steps = marks_to_do, go_around_barriers = false, markers = false)
-        move!(r,Nord)
-        marks_to_do -= 1
+function vec(robot, dir1, dir2, aa, bb, a, b):nothing
+    while (!isborder(robot, dir1) && !isborder(robot, dir2))
+        move!(robot, dir1)
+        move!(robot, dir2)
+        putmarker!(robot)
+        a += aa
+        b += bb
     end
-    go!(r, East; steps = marks_to_do, go_around_barriers = false, markers = true)
-    go!(r, West; steps = marks_to_do, go_around_barriers = false, markers = false)
-
-    go_to_west_south_corner_and_return_path!(r; go_around_barriers = false)
-    go_by_path!(r, path)
+    return a, b
 end
 
+function back(robot, a, b):nothing
+    if a < 0
+        dir = Sud
+    else
+        dir = Nord
+    end
+    while a != 0
+        move!(robot, dir)
+        if a > 0
+            a -= 1
+        else
+            a += 1
+        end
+    end
+
+    if b < 0
+        dir = Ost
+    else
+        dir = West
+    end
+    while b != 0
+        move!(robot, dir)
+        if b > 0
+            b -= 1
+        else 
+            b += 1
+        end
+    end
+    return a, b
+end
+
+function res(robot):nothing
+    a = 0
+    b = 0
+    putmarker!(robot)
+    a, b = vec(robot, Nord, Ost, -1, 1, a, b)
+    a, b = vec(robot, Sud, West, 1, -1, a, b)
+    a, b = back(robot, a, b)
+    a, b = vec(robot, Sud, Ost, 1, 1, a, b)
+    a, b = vec(robot, Nord, West, -1, -1, a, b)
+    a, b = back(robot, a, b)
+end
