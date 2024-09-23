2f81a6eefb5b29d1848cfefb3058d71621d25805
etrnldspr <92818272+etrnldspr@users.noreply.github.com>
2024-09-23 22:53:14 +0300

    Update lab_3.jl

--- lab_3.jl
+++ lab_3.jl
@@ -1,15 +1,37 @@
-function lab_3(r::Robot)
-    path = go_to_west_south_corner_and_return_path!(r)
-
-    side = East
-    while !isborder(r,North)
-        go_to_border_and_return_path!(r, side; markers = true)
-        side = inverse_side(side)
-        go!(r, North)
+function result!(robot):Nothing
+    a = 0
+    b = 0
+    while !isborder(robot, Nord)
+        move!(robot, Nord)
+        a += 1
     end
-
-    go_to_border_and_return_path!(r, side; markers = true)
-
-    go_to_west_south_corner_and_return_path!(r)
-    go_by_path!(r, path)
+    while !isborder(robot, West)
+        move!(robot, West)
+        b += 1
+    end
+    while (!isborder(robot, Sud) || !ismarker(robot))
+        putmarker!(robot)
+        while !isborder(robot, Ost)
+            move!(robot, Ost)
+            putmarker!(robot)
+        end
+        while !isborder(robot, West)
+            move!(robot, West)
+            putmarker!(robot)
+        end
+        if !isborder(robot, Sud)
+            move!(robot, Sud)
+        end
+    end
+    while !isborder(robot, Nord)
+        move!(robot, Nord)
+    end
+    while a > 0
+        move!(robot, Sud)
+        a -= 1
+    end
+    while b > 0
+        move!(robot, Ost)
+        b -= 1
+    end
 end
