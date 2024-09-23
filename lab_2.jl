9f918d289b33e36b77274257b499f49c9312e8b1
etrnldspr <92818272+etrnldspr@users.noreply.github.com>
2024-09-23 22:52:57 +0300

    lab_2.jl

--- lab_2.jl
+++ lab_2.jl
@@ -1,9 +1,37 @@
-function lab_2(r::Robot)
-    path = go_to_west_south_corner_and_return_path!(r)
-
-    for i ∈ (North, East, South, West)
-        go_to_border_and_return_path!(r, i; markers = true)
+function result!(robot):Nothing
+    a = 0
+    b = 0
+    while !isborder(robot, Nord)
+        move!(robot, Nord)
+        a += 1
     end
-    go_by_path!(r, path)
-    
+    while !isborder(robot, West)
+        move!(robot, West)
+        b += 1
+    end
+    putmarker!(robot)
+    while !isborder(robot, Ost)
+        putmarker!(robot)
+        move!(robot, Ost)
+    end
+    while !isborder(robot, Sud)
+        putmarker!(robot)
+        move!(robot, Sud)
+    end
+    while !isborder(robot, West)
+        putmarker!(robot)
+        move!(robot, West)
+    end
+    while !isborder(robot, Nord)
+        putmarker!(robot)
+        move!(robot, Nord)
+    end
+    while a > 0
+        move!(robot, Sud)
+        a -= 1
+    end
+    while b > 0
+        move!(robot, Ost)
+        b -= 1
+    end
 end
