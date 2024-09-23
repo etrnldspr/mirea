0b7e286d66199fb847e962626bb52ca13b7d5617
etrnldspr <92818272+etrnldspr@users.noreply.github.com>
2024-09-23 22:49:21 +0300

    1

--- lab_1.jl
+++ lab_1.jl
@@ -1,6 +1,32 @@
-function lab_1(r::Robot)   
-    for i ∈ (North, South, West, East)
-        go_to_border_come_back_and_return_distance!(r, i; markers = true)
+function result!(robot):Nothing
+    a = 0
+    putmarker!(robot)
+    while !isborder(robot, Nord)
+        move!(robot, Nord)
+        putmarker!(robot)
+        a -= 1
     end
-    putmarker!(r)
+    while !isborder(robot, Sud)
+        move!(robot, Sud)
+        putmarker!(robot)
+        a += 1
+    end
+    while a > 0
+        move!(robot, Nord)
+        a -= 1
+    end
+    while !isborder(robot, West)
+        move!(robot, West)
+        putmarker!(robot)
+        a -= 1
+    end
+    while !isborder(robot, Ost)
+        move!(robot, Ost)
+        putmarker!(robot)
+        a += 1
+    end
+    while a > 0
+        move!(robot, West)
+        a -= 1
+    end
 end
